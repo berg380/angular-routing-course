cfe657b93fc6b3689698244b79ae22eb75506ab7
berg380 <berg380@live.nl>
2024-11-24 09:24:40 +0100

    Completed module 'Routing in Standalone Component Applications'

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-  bootstrapApplication(AppComponent, appConfig);
+
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
