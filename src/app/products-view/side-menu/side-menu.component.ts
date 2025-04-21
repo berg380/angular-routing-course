b4982add926c3b54655767d31f855bcf86908337
berg380 <berg380@live.nl>
2025-04-21 11:33:04 +0200

    Completed module 'Creating Nested Router Outlets and Auxiliary Routes'

--- src/app/products-view/side-menu/side-menu.component.ts
+++ src/app/products-view/side-menu/side-menu.component.ts
@@ -3,6 +3,7 @@ import { PieService } from '../../services/pie.service';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { tap } from 'rxjs';
+import { PRODUCT_ROUTER_TOKENS } from '../products.routes';
 
 @Component({
   standalone: true,
@@ -20,7 +21,9 @@ import { tap } from 'rxjs';
 export class SideMenuComponent {
   @Input() customize = false;
   pies$ = this.pieService.filteredPies$;
-
+  readonly customizeLInk =  `./${PRODUCT_ROUTER_TOKENS.COSTOMIZE}`;
+  readonly detialLink = `./${PRODUCT_ROUTER_TOKENS.DETAIL}`;
+  
   constructor(
     private readonly pieService: PieService,
   ) {}
