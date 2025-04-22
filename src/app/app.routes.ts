1ba690002c66f60537470396ea560c2e005d55c7
berg380 <berg380@live.nl>
2025-04-22 13:49:22 +0200

    Completed module 9 'Control User Navigation with Route Guards'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CartAuthGuard } from './cart-auth-route-guard';
+import { HomeUpdatedComponent } from './home-updated/home-updated.component';
+import { inject } from '@angular/core';
+import { FeatureFlagService } from './services/feature-flag.service';
+import { map } from 'rxjs';
+import { authRouteGuard } from './cart-auth-route-guard';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
+import { NotReadyComponent } from './not-ready/not-ready.component';
+import { HelloService } from './services/hello.service';
 
 export enum ROUTER_TOKENS {
   HOME = 'home',
@@ -22,15 +29,35 @@ export const ROUTES: Routes = [
   },
   {
     path: ROUTER_TOKENS.HOME,
+    component: HomeUpdatedComponent,
+    canMatch: [() => {
+      const featureService = inject(FeatureFlagService);
+      return featureService.featureFlags.pipe(map(flags => !!flags.home))
+    }]
+  },
+  {
+    path: ROUTER_TOKENS.HOME,
     component: HomeComponent,
   },
   {
-    path: `${ROUTER_TOKENS.SHOP}/:categoryId` ,
+    path: `${ROUTER_TOKENS.SHOP}/:categoryId`,
     loadChildren: () => import('./products-view/products.routes').then(m => m.PRODUCT_ROUTES),
   },
   {
     path: ROUTER_TOKENS.CONTACT,
-    loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent)
+    loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent),
+    canActivate: [() => {
+      const featureService = inject(FeatureFlagService);
+      const router = inject(Router);
+      return featureService.featureFlags.pipe(map(flags => !!flags.contact || router.parseUrl(`/${ROUTER_TOKENS.NOT_READY}`)))
+    },
+    authRouteGuard(ROUTER_TOKENS.CONTACT)],
+    resolve: {
+      userHello: () => {
+        const helloService = inject(HelloService);
+        return helloService.getUserHello();
+      }
+    }
   },
   {
     path: ROUTER_TOKENS.ABOUT,
@@ -40,7 +67,15 @@ export const ROUTES: Routes = [
     path: ROUTER_TOKENS.CHECKOUT,
     outlet: ROUTER_TOKENS.CART,
     loadComponent: () => import('./cart/cart.component').then(m => m.CartComponent),
-    canActivate: [CartAuthGuard]
+    canActivate: [authRouteGuard(ROUTER_TOKENS.CART)]
+  },
+  {
+    path: ROUTER_TOKENS.NOT_AUTH,
+    component: NotAuthorizedComponent,
+  },
+  {
+    path: ROUTER_TOKENS.NOT_READY,
+    component: NotReadyComponent,
   },
   {
     path: '**',
